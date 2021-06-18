d162426fbe4e68aede1e5e31c87c6ecd5475e7d6
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-06-18 06:53:30 +0530

    init project commit

---
+++ src/main/java/com/hardik/meeseeks/SpringBootMovieDataFinderApplication.java
@@ -0,0 +1,13 @@
+package com.hardik.meeseeks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMovieDataFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMovieDataFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hardik/meeseeks/SpringBootMovieDataFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardik.meeseeks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMovieDataFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
