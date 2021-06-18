483a2f829d76f9a3437ddd27a4950e4a08acf826
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-06-18 08:31:33 +0530

    handing not found response

--- src/main/java/com/hardik/meeseeks/constant/Template.java
+++ src/main/java/com/hardik/meeseeks/constant/Template.java
@@ -4,5 +4,6 @@ public class Template {
 
 	public static final String INDEX = "index";
 	public static final String RESULT = "result";
+	public static final String NOT_FOUND = "not-found";
 
 }
--- src/main/java/com/hardik/meeseeks/controller/ResultController.java
+++ src/main/java/com/hardik/meeseeks/controller/ResultController.java
@@ -1,5 +1,6 @@
 package com.hardik.meeseeks.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,9 @@ public class ResultController {
 	@PostMapping("/result")
 	public String result(@RequestParam("title") final String movieTitle, final Model model) {
 		final var response = movieService.find(movieTitle);
+
+		if (response.getStatusCode().equals(HttpStatus.NOT_FOUND))
+			return Template.NOT_FOUND;
 
 		model.addAttribute("movie", movieService.find(movieTitle).getBody());
 		model.addAttribute("status", "success");
